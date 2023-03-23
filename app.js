58e66f4382069523f96bf20f4adf1dd2b5b19fe0
Azri Fadzil <afwcxx@gmail.com>
2023-03-23 17:39:34 +0800

    cors fix?

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ const express = require('express');
 const path = require('path');
 const logger = require('morgan');
 const helmet = require('helmet');
+const cors = require('cors')
 
 const indexRouter = require('./routes/index');
 const utxoRouter = require('./routes/utxo');
 
 const app = express();
 
+app.use(cors());
 app.use(helmet({
   contentSecurityPolicy: false
 }));
