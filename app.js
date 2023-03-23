73911c2dc1b9b754b8f15af553bf3c32f1e64ae6
Azri Fadzil <afwcxx@gmail.com>
2023-03-23 18:36:03 +0800

    workaround certificate check

--- app.js
+++ app.js
@@ -19,6 +19,17 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  if ('OPTIONS' === req.method) {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
+
 app.use('/', indexRouter);
 app.use('/utxo', utxoRouter);
 
